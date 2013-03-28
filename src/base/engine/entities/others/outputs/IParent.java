23f084c656ba893a356cbd4842c7f754ca64e5b0
Blackdread <jojo.julien@gmail.com>
2013-03-28 21:03:13 +0100

    trigger

--- src/base/engine/entities/others/outputs/IParent.java
+++ src/base/engine/entities/others/outputs/IParent.java
@@ -13,4 +13,26 @@ public interface IParent {
 	public void addChild(InputsAndOutputs child);
 	public void removeChild(InputsAndOutputs child);
 	
+	/**
+	 * Move with this entity. See Entity Hierarchy (parenting)
+	 */
+	public void SetParent(String parent);
+	/**
+	 * Change this entity to attach to a specific attachment point on its parent
+	 * The entity will teleport so that the position of its root bone matches that of the attachment
+	 * Entities must be parented before being sent this input
+	 */
+	public void SetParentAttachment(String parent);
+	/**
+	 * As above, but without teleporting
+	 * The entity retains its position relative to the attachment at the time of the input being received
+	 * @param parent name of the parent
+	 */
+	public void SetParentAttachmentMaintainOffset(String parent);
+	 
+	/**
+	 * Removes this entity from the the movement hierarchy, leaving it free to move independently
+	 */
+	public void ClearParent();
+	
 }
--- src/base/engine/entities/others/triggers/TriggerObjectInZone.java
+++ src/base/engine/entities/others/triggers/TriggerObjectInZone.java
@@ -56,20 +56,75 @@ public class TriggerObjectInZone extends Trigger {
 	/*
 	 * Inputs
 	 */
-	
+	/**
+	 * Tests if the trigger is being touched and fires an output based on whether the value is true or false
+	 * Triggers either the OnTouching or OnNotTouching outputs for whether anything is touching this entity
+	 */
+	public void TouchTest(){
+			
+	}
+
 	
 	/*
 	 * Outputs
 	 */
-	
-	
-	
-	
-	public ArrayList<BasicEntity> getArrayEntityToActON(){
-		return arrayEntityToActON;
+	/**
+	 * Fired when an entity stops touching this trigger
+	 * Only entities that passed this trigger's filters will cause this output to fire. (activator is the exiting entity)
+	 */
+	protected void OnEndTouch(){
+		
 	}
-	public void setArrayEntityToActON(ArrayList<BasicEntity> arrayEntity) {
-		this.arrayEntityToActON = arrayEntity;
+	/**
+	 * Fires when the last entity in the entity's area exits this trigger or when this entity is disabled
+	 * Only entities that passed this trigger's filters are considered. (activator is the last exiting entity)
+	 */
+	protected void OnEndTouchAll(){
+		
+	}
+	/**
+	 * Fired when the TouchTest input is called
+	 */
+	protected void OnTouching(){
+		
+	}
+	/**
+	 * Fired when the TouchTest input is called
+	 */
+	protected void OnNotTouching(){
+		
+	}
+	/**
+	 * Fired when an entity starts touching this trigger
+	 * The touching entity must pass this trigger's filters to cause this output to fire. (activator is the toucher)
+	 */
+	protected void OnStartTouch(){
+		
+	}
+	/**
+	 * Fired whenever the trigger is activated. (activator is the activator)
+	 */
+	protected void OnTrigger(){
+		
+	}
+	
+	
+	
+	/**
+	 * Devra ajouter l'entite et declencler les outputs OnStartTouch
+	 * @param entity
+	 */
+	public void addAnEntityToActON(BasicEntity entity) {
+		// TODO
+		arrayEntityToActON.add(entity);
+	}
+	/**
+	 * Devra enlever l'entite et declencler les outputs OnEndTouch
+	 * @param entity
+	 */
+	public void removeAnEntityToActON(BasicEntity entity) {
+		// TODO
+		arrayEntityToActON.add(entity);
 	}
 
 }
